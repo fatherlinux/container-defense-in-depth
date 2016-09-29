ac4fca6662b62810918cda33381807d02412a7a2
root <root@rhel7.dc2.crunchtools.com>
2016-09-29 13:26:58 -0400

    SM: Working first demo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # container-defense-in-depth
+This repository contains demos which support the Containers Defense in Depth presentation:
+bit.ly/containers-did
---
+++ demo-read-only.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Written by: Scott McCarty
+# Email: smccarty@redhat.com
+# Description: This code will demonstrate read only containers in action and free logging of data access.
+#              These commands can be ran manually or automatically with this script.
+#
+# Demo #1
+# This demonstrates read only containers, bind mounts (known data), and svirt
+
+# Configure auditd
+echo "# Monitor read-access of container mount point
+-w /mnt/container01 -p rwxa" > /etc/audit/rules.d/container.rules
+service auditd restart
+echo ""
+
+# Start the container
+CONTAINER_ID=`docker run -dt --read-only -v /mnt/container01/:/mnt/container01:Z rhel7 bash`
+echo ""
+
+# Cannot touch a file in temp
+docker exec $CONTAINER_ID touch /tmp/scott
+echo ""
+
+# Now touch a file on the bind mount
+docker exec $CONTAINER_ID touch /mnt/container01/test
+echo ""
+
+# Check the log
+ausearch -c touch -f "/mnt/container01/test" --start recent --end now -i --just-one
+echo ""
+
+# Stop the container
+docker kill $CONTAINER_ID
+echo ""
